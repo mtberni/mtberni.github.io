d5c529da52bc6367209fc3f9d1f11177ebe7d347
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-07-18 19:27:01 -0300

    mtberni

--- README.md
+++
@@ -1,1 +0,0 @@
-# mtberni.github.io
---
+++ contact_process.php
@@ -0,0 +1,38 @@
+<?php
+
+    $to = "mtbernioficial@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $csubject = $_REQUEST['subject'];
+    $cmessage = $_REQUEST['message'];
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $subject = "Você tem uma mensagem da aba de contato do Site.";
+
+    $logo = 'images/logo.png';
+    $link = 'www.mtberni.github.io';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Nome:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Assunto:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'><strong>Mensagem:</strong>{$cmessage}</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+    header('location: /index.html#contato');
+
+?>
