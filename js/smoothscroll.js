b5060bd71bd650ac0d3fe3689b2940211c741b6c
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-07-18 19:45:13 -0300

    Update smoothscroll.js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -301,3 +301,16 @@ function ssc_init() {
     ssc_addEvent("mousewheel", ssc_wheel);
     ssc_addEvent("load", ssc_init)
   }
+
+  $('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
